127fe7cb41c76510ec07ed38848055120f4ef8a3
mattdesl <dave.des@gmail.com>
2015-09-30 15:50:05 -0400

    fix typo

--- index.js
+++ index.js
@@ -158,7 +158,7 @@ function Controller(opts) {
 	this.isPanning = regionController.isPanning;
 	this.setPinchEnabled = regionController.setPinchEnabled.bind(regionController);
 	this.isPinchEnabled = regionController.isPinchEnabled.bind(regionController);
-	this.setWheenEnabled = regionController.setWheenEnabled.bind(regionController);
+	this.setWheelEnabled = regionController.setWheelEnabled.bind(regionController);
 	this.isWheelEnabled = regionController.isWheelEnabled.bind(regionController);
 }
 module.exports = Controller;
